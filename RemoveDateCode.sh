3673fc17d3fd9eb8a2befa10f0a31c160b58d320
John F. Nash <gianluca@johnrnash.home>
2018-01-19 19:05:48 -0500

    Wrapper.R --> Wrapper.sh

--- RemoveDateCode.sh
+++ RemoveDateCode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in CondPermSim.R ExtraRFCode.R GGGParty.R MeanSeOverRepeats.R ResGGGNew.R TestAgain.R TestMethods.R TheoreticalRSquared.R Variable_Selektion_ApproachesGGG.R Wrapper.R Log.txt; do 
+for i in CondPermSim.R ExtraRFCode.R GGGParty.R MeanSeOverRepeats.R ResGGGNew.R TestAgain.R TestMethods.R TheoreticalRSquared.R Variable_Selektion_ApproachesGGG.R Wrapper.sh Log.txt; do 
 	rm -rf ???????????????$i
 done
 
