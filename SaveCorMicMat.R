6d22240d071aad4fea4d02364343140ba47f8e5e
John F. Nash <gianluca@nbp-250-132.nbp.ruw.rutgers.edu>
2018-01-31 16:11:00 -0500

    Updated.

--- SaveCorMicMat.R
+++ SaveCorMicMat.R
@@ -1,13 +1,14 @@
 require(minerva)
-require(party)
+require(coin)
 
 PvalCalc = function(x1, x2) {
 	res  = 1-pvalue(independence_test(x1~x2, teststat="quad"))
 	return(res)
 }
 
+savePath="./CorMicPvalMatrices/"
 fileList = list.files()
-fileList = fileList[grepl("RData", fileList)]
+fileList = fileList[grepl(".RData", fileList)]
 for(i in fileList){
 	load(i)
 	xColIdx = 1:(ncol(dat)-1)
@@ -21,15 +22,15 @@ for(i in fileList){
 	} else{
 		corMatDatX = micMatDatX = "dataHasFactors"
 		pValMatDatX = matrix(0, nrow=max(xColIdx), ncol =max(xColIdx))
-		for(i in 2:ncol(pValMatDatX)) {
-			for (j in 1:(i-1)) {
-					depVal = PvalCalc(X[,i], X[,j], pValuePower)		
-				pValMatDatX[i,j] = depVal
-				pValMatDatX[j,i] = weightMat[i,j]
+		for(k in 2:ncol(pValMatDatX)) {
+			for (j in 1:(k-1)) {
+					depVal = PvalCalc(dat[,k], dat[,j])		
+				pValMatDatX[k,j] = depVal
+				pValMatDatX[j,k] = pValMatDatX[k,j]
 			}
 		}	
 	}	
-	save(dat, corMatDatX, micMatDatX, pValMatDatX, file=paste0("CorMicPvalMat", i))
+	save(dat, corMatDatX, micMatDatX, pValMatDatX, file=paste0(savePath, "CorMicPvalMat", i))
 	rm(dat, corMatDatX, micMatDatX, pValMatDatX)
 	print(i)
 }
