da2a1eb4b4c6eee6451e19a6def751bb094441dc
John F. Nash <gianluca@nbp-250-132.nbp.ruw.rutgers.edu>
2018-01-31 15:46:05 -0500

    Added pVal mat.

--- SaveCorMicMat.R
+++ SaveCorMicMat.R
@@ -1,13 +1,35 @@
+require(minerva)
+require(party)
+
+PvalCalc = function(x1, x2) {
+	res  = 1-pvalue(independence_test(x1~x2, teststat="quad"))
+	return(res)
+}
+
 fileList = list.files()
 fileList = fileList[grepl("RData", fileList)]
 for(i in fileList){
 	load(i)
-	corMatDatX = cor(dat[,1:(ncol(dat)-1)])
-	if(ncol(dat)>500)
-		micMatDatX="dataIsTooLarge"
-	else
-		micMatDatX = mine(dat[,1:(ncol(dat)-1)])$MIC
-	save(dat, corMatDatX, micMatDatX, file=paste0("CorMicMat", i))
-	rm(dat, corMatDatX, micMatDatX)
+	xColIdx = 1:(ncol(dat)-1)
+	if(!any(sapply(dat[,xColIdx], is.factor))){
+		pValMatDatX = "dataIsNumeric"
+		corMatDatX = cor(dat[,xColIdx])
+		if(ncol(dat)>500)
+			micMatDatX="dataIsTooLarge"
+		else
+			micMatDatX = mine(dat[,xColIdx])$MIC
+	} else{
+		corMatDatX = micMatDatX = "dataHasFactors"
+		pValMatDatX = matrix(0, nrow=max(xColIdx), ncol =max(xColIdx))
+		for(i in 2:ncol(pValMatDatX)) {
+			for (j in 1:(i-1)) {
+					depVal = PvalCalc(X[,i], X[,j], pValuePower)		
+				pValMatDatX[i,j] = depVal
+				pValMatDatX[j,i] = weightMat[i,j]
+			}
+		}	
+	}	
+	save(dat, corMatDatX, micMatDatX, pValMatDatX, file=paste0("CorMicPvalMat", i))
+	rm(dat, corMatDatX, micMatDatX, pValMatDatX)
 	print(i)
 }
